6a2853ca983c92620ec367214db72a452a2b4d46
soulshake <amy@gandi.net>
2014-09-08 20:47:01 -0700

    clean up setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,8 @@ setup(
     author='AJ Bowen',
     author_email='aj@gandi.net',
     packages=['idncheck', 'idncheck.tests'],
-    scripts=['bin/nothing.py','bin/more_nothing.py'],
-    url='http://example.com/',
     license='LICENSE.txt',
     description='Returns IDN restriction level of a string.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "Nothing >= 1.1.1",
-        "somepackage == 0.1.4",
-    ],
+    long_description=open('README.md').read()
 )
 
